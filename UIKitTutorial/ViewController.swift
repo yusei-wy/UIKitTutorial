859cee38da32a7969a1f931b106cb56f4b9f855b
yusei-wy <yusei.kasa@gmail.com>
2024-07-09 20:37:36 +0900

    Add label

--- UIKitTutorial/ViewController.swift
+++ UIKitTutorial/ViewController.swift
@@ -12,5 +12,9 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     view.backgroundColor = .systemBlue
+    let label = UILabel()
+    label.text = "Hello UIKit"
+    label.frame = CGRect(x: 100, y: 100, width: 200, height: 100)
+    view.addSubview(label)
   }
 }
